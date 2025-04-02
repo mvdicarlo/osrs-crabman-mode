081487de25cb29f80baa39ecfba9d8c21659ae31
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2025-04-02 09:24:17 -0400

    feat: adds similar side panel from anotherbronzemanmode

---
+++ src/main/java/mvdicarlo/crabmanmode/ItemObject.java
@@ -0,0 +1,13 @@
+package mvdicarlo.crabmanmode;
+
+import lombok.Value;
+import net.runelite.client.util.AsyncBufferedImage;
+
+@Value
+public class ItemObject
+{
+    int id;
+    String name;
+    boolean tradeable;
+    AsyncBufferedImage icon;
+}
